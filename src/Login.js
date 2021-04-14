00c28e3251a32b6ab235680760f1620c54fb429b
Joseph Passalacqua <ec2-user@ip-172-31-53-194.ec2.internal>
2021-04-14 19:16:45 +0000

    Got api to work for google sign in. Starting to create a form to login ort create an account

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,43 @@
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
+
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 export function Login() {
     return(
         <div class="login">
-            <h1> Login here: </h1>
+            <head>
+                <title>Penalty chess login page</title>
+                <meta name="google-signin-client_id" content="343458998580-grsva1siatfujrucu7b75hug4hocopsg.apps.googleusercontent.com"/>
+                <script src="https://apis.google.com/js/platform.js" async defer></script>
+            </head>
+            
+            <body>
+                <h1> Login here: </h1>
+                <div class="google">
+                    <div class = "box">
+                <GoogleLogin
+                        buttonText="Login"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                <br/>
+                <br/>
+                <form id="login-form">
+                    <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username"/>
+                    <br/>
+                    <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password"/>
+                    <br/>
+                    <input type="submit" value="Login" id="login-form-submit"/>
+                    <input type="submit" value="Create Account" />
+                </form>
+                    </div>
+                </div>
+            </body>
         </div>
 );
 }
